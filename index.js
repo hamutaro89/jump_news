e6e783d5073f44791dd2c5b1be11684ba93a7081
r84190629 <raymond.chai@huawei.com>
2023-03-02 16:07:27 +0800

    crawlUrl

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
+import express from 'express'
+import { callPuppeteer } from './controller.js';
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
+app.get('/crawlUrl', callPuppeteer)
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
