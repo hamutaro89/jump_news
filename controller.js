ebd2eff3911067978398e0fa9e7e97d04c624d64
r84190629 <raymond.chai@huawei.com>
2023-03-02 17:05:24 +0800

    dockerfile

--- controller.js
+++ controller.js
@@ -1,7 +1,16 @@
 import puppeteer from 'puppeteer';
+require("dotenv").config();
 
 const scrapeLogic = async function(req, res){
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath: process.env.NODE_ENV == 'production' ? process.env.PUPPETEER_PATH : puppeteer.executablePath()
+  });
   try {    
     const page = await browser.newPage();
 
