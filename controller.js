ae3c909fd08f4da799cb02ba6ad06202c2fd9f7e
r84190629 <raymond.chai@huawei.com>
2023-03-03 13:29:04 +0800

    added google, petal, straitstimes

--- controller.js
+++ controller.js
@@ -32,10 +32,9 @@ const scrapeLogic = async function(req, res){
     const textSelector = await page.waitForSelector(
       'text/Customize and automate'
     );
+    console.log(textSelector);
     const fullTitle = await textSelector.evaluate(el => el.textContent);
-
-    // Print the full title
-    console.log('The title of this blog post is "%s".', fullTitle);    
+ 
     res.send(fullTitle);
     res.status(200);
   } catch (error) {
@@ -46,4 +45,122 @@ const scrapeLogic = async function(req, res){
   }
 };
 
-export { scrapeLogic };
+async function matchGoogle(req, res){
+  let data = req.body;
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath: process.env.NODE_ENV == 'production' ? process.env.PUPPETEER_PATH : puppeteer.executablePath()
+  });
+  let result = [];
+  try {
+    const page = await browser.newPage();
+    for(let d of data){
+      try{        
+        await page.goto(`https://www.google.com/search?q=${d.title}`, { timeout: 90000, waitUntil: "networkidle2" }); 
+        try{
+          await page.waitForSelector('.QXROIe', { timeout: 1000 }).then( async () => {
+            result.push({
+              _id: d._id,
+              google: true
+            })
+          });
+        }catch(err){
+          result.push({
+            _id: d._id,
+            google: false
+          }) 
+        }
+      }catch(err){
+        console.log(err)   
+      }
+    }    
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Error", error);
+  } finally{
+    await browser.close();
+  }
+  res.status(200).send(result);
+}
+
+async function matchPetal(req, res){
+  let data = req.body;
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath: process.env.NODE_ENV == 'production' ? process.env.PUPPETEER_PATH : puppeteer.executablePath()
+  });
+  let result = [];
+  try {
+    const page = await browser.newPage();
+    for(let d of data){
+      try{        
+        await page.goto(`https://www.petalsearch.com/search?query=${d.title}`, { timeout: 90000, waitUntil: "networkidle2" }); 
+        try{
+          await page.waitForSelector('.news-card', { timeout: 1000 }).then( async () => {
+            result.push({
+              _id: d._id,
+              petal: true
+            })
+          });
+        }catch(err){
+          result.push({
+            _id: d._id,
+            petal: false
+          }) 
+        }
+      }catch(err){
+        console.log(err)   
+      }
+    }    
+  } catch (error) {
+    console.log(error);
+    res.send("Error", error);
+  } finally{
+    await browser.close();
+  }
+  res.status(200).send(result);
+}
+
+async function straitsTimes(req, res){
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath: process.env.NODE_ENV == 'production' ? process.env.PUPPETEER_PATH : puppeteer.executablePath()
+  });
+  let result = null;
+  try {
+    const page = await browser.newPage();
+    try {
+      await page.goto(`https://www.straitstimes.com/singapore`, { timeout: 90000, waitUntil: "networkidle2" });
+      result = await page.evaluate(() => {
+        const element = document.querySelector('.block-block-most-popular');
+        return element.outerHTML;
+      });
+    } catch (error) {
+      console.log(error);
+      res.send("Error", error);
+    }  
+  } catch (error) {
+    console.log(error);
+    res.send("Error", error);
+  } finally{
+    await browser.close();
+  }
+  res.status(200).send(result);
+}
+
+export { scrapeLogic, matchGoogle, matchPetal, straitsTimes };
--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 import express from 'express'
-import bodyParser from 'body-parser'
-import { scrapeLogic } from './controller.js';
+import { scrapeLogic, matchGoogle, matchPetal, straitsTimes } from './controller.js';
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.get('/scrapeUrl', scrapeLogic)
+
+app.get('/scrapeUrl', scrapeLogic);
+
+app.post('/scrape', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.post('/straitsTimes', straitsTimes);
+app.post('/matchGoogle', matchGoogle);
+app.post('/matchPetal', matchPetal);
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
