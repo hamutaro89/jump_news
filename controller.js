6a8f1a437ad5c2f02551b6e39ecfee5a16080129
r84190629 <raymond.chai@huawei.com>
2023-04-18 11:27:36 +0800

    test

--- controller.js
+++ controller.js
@@ -8,14 +8,22 @@ async function scrapeLogic(req, res){
   try {    
     const page = await browser.newPage();
 
-    await page.goto('https://www.straitstimes.com/singapore');
+    await page.goto('https://developer.chrome.com/');
 
     // Set screen size
     await page.setViewport({width: 1080, height: 1024});
 
+    // Type into search box
+    await page.type('.search-box__input', 'automate beyond recorder');
+
+    // Wait and click on first result
+    const searchResultSelector = '.search-box__link';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+
     // Locate the full title with a unique string
     const textSelector = await page.waitForSelector(
-      '.block-block-most-popular'
+      'text/Customize and automate'
     );
     console.log(textSelector);
     const fullTitle = await textSelector.evaluate(el => el.textContent);
